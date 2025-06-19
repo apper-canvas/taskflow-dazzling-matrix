e09116039ca377576938ee47df71a908f91a232b
RishabhCompanyHub <rishabh@companyhub.com>
2025-06-19 19:03:20 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,7 @@ function Login() {
           </div>
           <div className="flex flex-col gap-1 items-center justify-center">
             <div className="text-center text-lg xl:text-xl font-bold">
-              Sign in to TaskFlow Pro
+              Sign in to TaskFlow Pro 1
             </div>
             <div className="text-center text-sm text-gray-500">
               Welcome back, please sign in to continue
